a67fb94e42cfcf7373d1b1145a592d8a73c41183
admin <admin@DHJW-iMac-2011-27inch.local>
2019-01-13 21:35:06 +0100

    .

---
+++ id0000146/android/app/src/main/kotlin/com/firedevelop/id0000146/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.firedevelop.id0000146
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
